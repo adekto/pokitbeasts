67e13b7375460b2017222b964862dbfd23b6e5ae
adekto <iwert.haex@gmail.com>
2018-08-18 17:20:30 +0200

    added drummyfish tileset
    added drummyfish tileset to overworld tileset

--- tilemaps/overworld.tsx
+++ tilemaps/overworld.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="overworld" tilewidth="16" tileheight="16" tilecount="81" columns="9">
- <image source="../img/overworld.png" width="144" height="144"/>
+<tileset name="overworld" tilewidth="16" tileheight="16" tilecount="144" columns="16">
+ <image source="../img/overworld.png" width="256" height="144"/>
 </tileset>
